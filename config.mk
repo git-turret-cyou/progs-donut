6d0bac97daa43dcc1d20cf726f7c9daa7160fbf9
turret <turret@duck.com>
2023-06-23 01:00:00 -0500

    better stylize the config.mk file

--- config.mk
+++ config.mk
@@ -1,6 +1,6 @@
 PREFIX = /usr/local
 
-CFLAGS= -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Os
+CFLAGS = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Os
 LDFLAGS = -lm -lX11
 
-CC =cc
+CC = cc
