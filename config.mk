700a3e787bd0a07171c38fd2b2b051c550ee35cf
turret <turret@duck.com>
2023-06-29 16:59:40 -0500

    config.mk: add cppflags to cflags

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 PREFIX = /usr/local
 
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700L -D_POSIX_C_SOURCE=200809L
-CFLAGS = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Os
+CFLAGS = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Os ${CPPFLAGS}
 LDFLAGS = -lm -lX11
 
 CC = cc
